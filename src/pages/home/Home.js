8bdb720882018f43d4c125118e45d5ce0ff2177d
Adrian Hervas <adrian.hervasmartinez@gmail.com>
2021-12-11 18:54:12 +0100

    Edit README

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,6 @@
 //Bootstrap components
-
-import Carousel from "react-bootstrap/Carousel";
-import Col from "react-bootstrap/esm/Col";
 import Container from "react-bootstrap/esm/Container";
-import Image from "react-bootstrap/esm/Image";
-import Row from "react-bootstrap/esm/Row";
+
 
 // Styles
 import './Home.css'
